8084613755f35d7cffb8cc5eba95939e91e5fb2c
Amy <maz2130@barnard.edu>
2018-01-12 11:01:10 -0500

    Add new files

---
+++ index.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+  $('.inline-code').on('click', function() {
+  $(this).html('wow thanks for following directions lol');
+  $(this).toggleClass('container');
+});
+//this is the same as .inline-code (it's referring to it)
+$('p').on('click', function() {
+  $(this).css({
+    "color": "#42cbad",
+    "font-weight": "700",
+    "line-height": "40px",
+    // "attribute": "value",
+  });
+});
+$('h3').on('mouseover', function() {
+  $(this)
+  .animate({
+    "letter-spacing": "10px",
+    "font-size": "40px"
+  }, 1000)
+  .animate({
+  	"letter-spacing": "5px",
+  	"font-size": "20px"
+  	}, 1000);
+});
+});
